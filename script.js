bf780dfaa53b657d38efca892bc1aa50130368b6
Eakansh Srivastava <you@example.com>
2021-09-30 01:10:08 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const stocks = document.querySelectorAll(".stock-data");
+
+const btn = document.querySelector("#btn");
+
+const output = document.querySelector("#output");
+
+function calculatePrice() {
+    
+}
+
+function calculateProfitOrLoss(initial, quantity, current)
+{
+    if(initial&&quantity&&current)
+    {
+        if(initial>current)
+        {
+            var loss = (initial-current) * quantity;
+            var lossPer = loss/initial * 100;
+            //console.log(`Hey, the loss is ${loss} and the loss % is ${lossPer}`)
+            output.innerHTML = "Hey, the loss is "+ loss + " and the loss % is "+ lossPer;
+        }
+        else if(current>initial)
+        {
+            var profit = (current-initial) * quantity;
+            var profitPer = profit/initial * 100;
+            //console.log(`Hey, the profit is ${profit} and the profit % is ${profitPer}`)
+            output.innerHTML = "Hey, the loss is "+ profit + " and the loss % is "+ profitPer;
+        }
+        else{
+            output.innerHTML = "No gain, no pain";
+        }
+    }
+    else
+    output.innerHTML = "Invalid input, try again";
+}
+
+function calculatePrice()
+{
+ calculateProfitOrLoss(Number(stocks[0].value),Number(stocks[1].value),Number(stocks[2].value))   
+}
+
+btn.addEventListener("click", calculatePrice)
